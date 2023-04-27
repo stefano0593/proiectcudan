3edfd70811059d734c88d9c5a2594bca9fd83aa0
stefano0593 <stefano0593@yahoo.com>
2023-04-27 21:10:25 +0300

    modificarea1

--- proiectreact/src/App.js
+++ proiectreact/src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <div>Sa plecam acasa dupa ce bem.</div>
         </p>
         <a
           className="App-link"
